0c583bef31f47f204a798315f2c6500d4df996c2
Jared Scarr <jaredscarr@pm.me>
2023-11-11 08:05:40 -0800

    Add Conway's game of life repository and updated README.md

---
+++ conways/src/main.py
@@ -0,0 +1,13 @@
+"""Conway's Game of Life Entry Point."""
+import pygame
+
+
+class GameOfLife:
+    """Game class for """
+
+    def __init__(self) -> None:
+        """Init game."""
+
+
+if __name__ == '__main__':
+    game = GameOfLife()
