637169f1568300e30e5855d72f490abea1a20bd6
Michael Duder <mgduder@icloud.com>
2023-11-13 10:20:40 -0800

    Refactor check_neighbors method
    The method was too arcane and unreadable with the previous approach. There is now a new method called check_cell_neighbors which takes a cell and just checks and counts them. The logic for wrapping is now in the Cell class which can now tell where its left, right, top, and bottom neighbors are

--- conways/src/main.py
+++ conways/src/main.py
@@ -2,9 +2,11 @@ import pygame
 import time
 from os import system
 from enum import Enum
+from typing import Tuple
 
 # grid size is height and width of grid
-GRID_SIZE = 20
+GRID_LEN = 20
+GRID_LAST = GRID_LEN - 1 
 
 # chars for printing game to console. change to something fun if you want!
 ALIVE = '⬜'
@@ -15,125 +17,6 @@ class OperationFlag(Enum):
     REVIVE = 0 
     KILL = 1
 
-# All cells belonging to grid. Key is the location.    
-cells = {}
-
-def check_on_neighbors(location) -> int:
-    """A function that counts how many of a cell's neighbors are alive.
-
-    Takes the location of the cell and returns the number of living cells.
-    
-    Wrapping is performed if any neighbors are beyond the grid's boundaries.
-    """
-
-    living = 0
-    (x,y) = location
-
-    # need to check cases where any neighbor might be on the other side of the grid
-    match [x - 1 < 0, x + 1 >= GRID_SIZE, y - 1 < 0, y + 1 >= GRID_SIZE]:
-        
-        # left top corner case
-        case [True, False, True, False]:
-            if cells[GRID_SIZE - 1, GRID_SIZE - 1].is_alive: living += 1
-            if cells[GRID_SIZE - 1, y].is_alive: living += 1
-            if cells[GRID_SIZE - 1, y + 1].is_alive: living += 1
-            if cells[x + 1, GRID_SIZE - 1].is_alive: living += 1
-            if cells[x + 1, y].is_alive: living += 1
-            if cells[x + 1, y + 1].is_alive: living += 1
-            if cells[x, GRID_SIZE - 1].is_alive: living += 1
-            if cells[x, y + 1].is_alive: living += 1
-        
-        # left bottom corner case
-        case [True, False, False, True]:
-            if cells[GRID_SIZE - 1, y - 1].is_alive: living += 1
-            if cells[GRID_SIZE - 1, y].is_alive: living += 1
-            if cells[GRID_SIZE - 1, 0].is_alive: living += 1
-            if cells[x + 1, y-1].is_alive: living += 1
-            if cells[x + 1, y].is_alive: living += 1
-            if cells[x + 1, 0].is_alive: living += 1
-            if cells[x, y - 1].is_alive: living += 1
-            if cells[x, 0].is_alive: living += 1
-        
-        # right bottom corner case
-        case [False, True, False, True]:
-            if cells[x - 1, y - 1].is_alive: living += 1
-            if cells[x - 1, y].is_alive: living += 1
-            if cells[x - 1, 0].is_alive: living += 1
-            if cells[0, y - 1].is_alive: living += 1
-            if cells[0, y].is_alive: living += 1
-            if cells[0, 0].is_alive: living += 1
-            if cells[x, y - 1].is_alive: living += 1
-            if cells[x, 0].is_alive: living += 1
-
-
-        # right top corner case
-        case [False, True, True, False]:
-            if cells[x - 1, GRID_SIZE - 1].is_alive: living += 1
-            if cells[x - 1, y].is_alive: living += 1
-            if cells[x - 1, y + 1].is_alive: living += 1
-            if cells[0, GRID_SIZE - 1].is_alive: living += 1
-            if cells[0, y].is_alive: living += 1
-            if cells[0, y + 1].is_alive: living += 1
-            if cells[x, GRID_SIZE - 1].is_alive: living += 1
-            if cells[x, y + 1].is_alive: living += 1
-
-        # left border case
-        case [True, False, False, False]:
-            if cells[GRID_SIZE - 1, y - 1].is_alive: living += 1
-            if cells[GRID_SIZE - 1, y].is_alive: living += 1
-            if cells[GRID_SIZE - 1, y + 1].is_alive: living += 1
-            if cells[x + 1, y - 1].is_alive: living += 1
-            if cells[x + 1, y].is_alive: living += 1
-            if cells[x + 1, y + 1].is_alive: living += 1
-            if cells[x, y - 1].is_alive: living += 1
-            if cells[x, y + 1].is_alive: living += 1
-        
-        # right border case
-        case [False, True, False, False]:
-            if cells[x - 1, y - 1].is_alive: living += 1
-            if cells[x - 1, y].is_alive: living += 1
-            if cells[x - 1, y + 1].is_alive: living += 1
-            if cells[0, y - 1].is_alive: living += 1
-            if cells[0, y].is_alive: living += 1
-            if cells[0, y + 1].is_alive: living += 1
-            if cells[x, y - 1].is_alive: living += 1
-            if cells[x, y + 1].is_alive: living += 1
-
-        # bottom border case
-        case [False, False, False, True]:
-            if cells[x - 1, y - 1].is_alive: living += 1
-            if cells[x - 1, y].is_alive: living += 1
-            if cells[x - 1, 0].is_alive: living += 1
-            if cells[x + 1, y - 1].is_alive: living += 1
-            if cells[x + 1, y].is_alive: living += 1
-            if cells[x + 1, 0].is_alive: living += 1
-            if cells[x, y - 1].is_alive: living += 1
-            if cells[x, 0].is_alive: living += 1
-        
-        # top border case
-        case [False, False, True, False]:
-            if cells[x - 1, GRID_SIZE - 1].is_alive: living += 1
-            if cells[x - 1, y].is_alive: living += 1
-            if cells[x - 1, y + 1].is_alive: living += 1
-            if cells[x + 1, GRID_SIZE - 1].is_alive: living += 1
-            if cells[x + 1, y].is_alive: living += 1
-            if cells[x + 1, y + 1].is_alive: living += 1
-            if cells[x, GRID_SIZE - 1].is_alive: living += 1
-            if cells[x, y + 1].is_alive: living += 1
-        
-        # standard case
-        case [False, False, False, False]:
-            if cells[x - 1, y - 1].is_alive: living += 1
-            if cells[x - 1, y].is_alive: living += 1
-            if cells[x - 1, y + 1].is_alive: living += 1
-            if cells[x + 1, y - 1].is_alive: living += 1
-            if cells[x + 1, y].is_alive: living += 1
-            if cells[x + 1, y + 1].is_alive: living += 1
-            if cells[x, y - 1].is_alive: living += 1
-            if cells[x, y + 1].is_alive: living += 1
-
-    return living
-
 class Cell:
     """Simple class that defines a cell.
     
@@ -143,11 +26,40 @@ class Cell:
     """
     
     # location of cell and whether it is alive or dead
-    __slots__= 'location', 'is_alive'
+    __slots__= 'x', 'y', 'is_alive'
 
-    def __init__(self, location) -> None:
-        self.location = location
+    def __init__(self, location: Tuple[int, int]) -> None:
+        (self.x, self.y) = location
         self.is_alive = False
+
+    def location(self):
+        return (self.x, self.y)
+
+    def get_left_neighbor_loc(self) -> Tuple[int, int]:
+        if self.x - 1 < 0:
+            return (GRID_LAST, self.y)
+        else:
+            return (self.x - 1, self.y)
+    
+    def get_right_neighbor_loc(self) -> Tuple[int, int]:
+        if self.x + 1 > GRID_LAST:
+            return (0, self.y)
+        else:
+            return (self.x + 1, self.y)
+
+    def get_top_neighbor_loc(self) -> Tuple[int, int]:
+        if self.y - 1 < 0:
+            return (self.x, GRID_LAST)
+        else:
+            return (self.x, self.y - 1)
+    
+    def get_bottom_neighbor_loc(self) -> Tuple[int, int]:
+        if self.y + 1 > GRID_LAST:
+            return (self.x, 0)
+        else:
+            return (self.x, self.y + 1)
+
+
             
  
 class Game:
@@ -155,27 +67,27 @@ class Game:
 
     # reference to initialized pygame screen
     __slots__ = 'screen'
+    cells = {}
 
     def __init__(self, title: str = "NSCCSC Life Clone") -> None:
         pygame.init()
         pygame.display.set_caption(title)
 
         # nested loop generates location, initializes a cell, and inserts it in the dict
-        for y in range(GRID_SIZE):
-            for x in range(GRID_SIZE):
-                cells[(x, y)] = Cell((x, y))
+        for y in range(GRID_LEN):
+            for x in range(GRID_LEN):
+                self.cells[(x, y)] = Cell((x, y))
 
-        self.screen = pygame.display.set_mode((800, 600))
+        #self.screen = pygame.display.set_mode((800, 600))
 
         # uncomment following code to put a glider on the grid
         # useful for running game without input
         #
-        # cells[(5,5)].is_alive = True
-        # cells[(6,6)].is_alive = True
-        # cells[(6,7)].is_alive = True
-        # cells[(5,7)].is_alive = True
-        # cells[(4,7)].is_alive = True
-       
+        #self.cells[(5,5)].is_alive = True
+        #self.cells[(6,6)].is_alive = True
+        #self.cells[(6,7)].is_alive = True
+        #self.cells[(5,7)].is_alive = True
+        #self.cells[(4,7)].is_alive = True
 
     def run(self) -> None:
         """Main loop of game.
@@ -186,14 +98,42 @@ class Game:
             self.process_game_logic()
             self.draw_game_elements()
 
+    def check_cell_neighbors(self, cell: Cell) -> int:
+        """ Checks how many neighbors are alive """
+        living = 0
+        
+        top_cell = self.cells[cell.get_top_neighbor_loc()]       
+        top_left_cell = self.cells[top_cell.get_left_neighbor_loc()]
+        top_right_cell = self.cells[top_cell.get_right_neighbor_loc()]
+        bottom_cell = self.cells[cell.get_bottom_neighbor_loc()]
+        bottom_left_cell = self.cells[bottom_cell.get_left_neighbor_loc()]
+        bottom_right_cell = self.cells[bottom_cell.get_right_neighbor_loc()]
+        left_cell = self.cells[cell.get_left_neighbor_loc()]
+        right_cell = self.cells[cell.get_right_neighbor_loc()]
+        
+
+        living = len(list(filter(lambda x: x, [
+            top_cell.is_alive, 
+            top_left_cell.is_alive,
+            top_right_cell.is_alive,
+            bottom_cell.is_alive,
+            bottom_left_cell.is_alive,
+            bottom_right_cell.is_alive,
+            left_cell.is_alive,
+            right_cell.is_alive
+        ])))
+
+        return living
+        
+
     def process_game_logic(self):
         """Function for updating game logic every frame."""
 
         changing = [] # references to all cells changing this frame and the outcome
 
         # for every cell check its neighbors and detirmine if its changing
-        for cell in cells.values():
-            living = check_on_neighbors(cell.location)
+        for cell in self.cells.values():
+            living = self.check_cell_neighbors(cell)
             if not (1 < living < 4): # death condition
                 changing.append((cell, OperationFlag.KILL))
             elif living == 3: # revive condition
@@ -215,10 +155,10 @@ class Game:
     def draw_in_console(self):
         """Method to see game in console"""
         system('clear')
-        for y in range(GRID_SIZE):
+        for y in range(GRID_LEN):
             print("", end="")
-            for x in range(GRID_SIZE):
-                cell = cells[(x,y)]
+            for x in range(GRID_LEN):
+                cell = self.cells[(x,y)]
                 if cell.is_alive:
                     print(ALIVE, end="")
                 else: 
@@ -226,13 +166,14 @@ class Game:
             print()
 
 
+
 if __name__ == "__main__":
     game = Game()
-    game.run()
+    #game.run()
 
     
     # Fake game loop for printing game to console
-    #while True:
-    #    game.draw_in_console()
-    #    game.process_game_logic()
-    #    time.sleep(0.1) # change to adjust print speed
+    while True:
+        game.draw_in_console()
+        game.process_game_logic()
+        time.sleep(0.1)
