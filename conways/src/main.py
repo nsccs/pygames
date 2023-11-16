b9bd0369b1366399848e017d70b5a0eeb178b620
Michael Duder <mgduder@icloud.com>
2023-11-16 13:39:16 -0800

    Change So display updates once per loop

--- conways/src/main.py
+++ conways/src/main.py
@@ -1,8 +1,7 @@
 import pygame
 import time
-from os import supports_effective_ids, system
-import sys
 from enum import Enum
+import random
 from typing import Tuple
 
 
@@ -10,7 +9,7 @@ SCREEN_SIZE = 1000
 
 file = open("log.txt", "w")
 # grid size is height and width of grid
-GRID_LEN = 1000
+GRID_LEN = 500
 GRID_LAST = GRID_LEN - 1
 
 class OperationFlag(Enum):
@@ -135,7 +134,8 @@ class Cell:
         else:
             pygame.draw.rect(screen, color, self.rect)
             #screen.blit(pygame.font.SysFont('Arial', 25).render(f"{self.living_neighbors}", True, (0,0,0)), (self.rect.x, self.rect.y))
-        pygame.display.update(self.rect)
+        return self.rect
+        #pygame.display.update(self.rect)
 
     def location(self):
         return (self.x, self.y)
@@ -160,24 +160,30 @@ class Game:
 
         glider = [(5, 5), (6, 6), (6, 7), (5, 7), (4, 7)]
         spaceship = [(34, 20), (37, 20), (38, 20), (45, 20), (48, 20), (49, 20), (50, 20), (26, 21), (30, 21), (31, 21), (32, 21), (33, 21), (35, 21), (36, 21), (37, 21), (38, 21), (39, 21), (40, 21), (45, 21), (48, 21), (49, 21), (50, 21), (22, 22), (23, 22), (24, 22), (25, 22), (26, 22), (31, 22), (36, 22), (41, 22), (42, 22), (43, 22), (21, 23), (28, 23), (29, 23), (31, 23), (38, 23), (39, 23), (41, 23), (42, 23), (43, 23), (46, 23), (48, 23), (49, 23), (50, 23), (22, 24), (23, 24), (24, 24), (25, 24), (26, 24), (28, 24), (29, 24), (30, 24), (39, 24), (40, 24), (41, 24), (42, 24), (46, 24), (48, 24), (49, 24), (50, 24), (26, 25), (29, 25), (44, 25), (29, 26), (30, 26), (41, 26), (42, 26), (44, 26), (45, 26), (29, 27), (30, 27), (41, 27), (42, 27), (44, 27), (45, 27), (26, 28), (29, 28), (44, 28), (22, 29), (23, 29), (24, 29), (25, 29), (26, 29), (28, 29), (29, 29), (30, 29), (39, 29), (40, 29), (41, 29), (42, 29), (46, 29), (48, 29), (49, 29), (50, 29), (21, 30), (28, 30), (29, 30), (31, 30), (38, 30), (39, 30), (41, 30), (42, 30), (43, 30), (46, 30), (48, 30), (49, 30), (50, 30), (22, 31), (23, 31), (24, 31), (25, 31), (26, 31), (31, 31), (36, 31), (41, 31), (42, 31), (43, 31), (26, 32), (30, 32), (31, 32), (32, 32), (33, 32), (35, 32), (36, 32), (37, 32), (38, 32), (39, 32), (40, 32), (45, 32), (48, 32), (49, 32), (50, 32), (34, 33), (37, 33), (38, 33), (45, 33), (48, 33), (49, 33), (50, 33)]
-
+        infinit = [(207, 200), (205, 201), (207, 201), (208, 201), (205, 202), (207, 202), (205, 203), (203, 204), (201, 205), (203, 205)]
+        time_bomb = [(2, 0), (14, 0), (15, 0), (1, 1), (3, 1), (8, 1), (15, 1), (8, 2), (13, 2), (3, 3), (6, 3), (10, 3), (13, 3), (3, 4), (4, 4), (11, 4), (4, 5)]
         # nested loop generates location, initializes a cell, and inserts it in the dict
         for y in range(GRID_LEN):
             for x in range(GRID_LEN):
                 Cell((x, y))
         
         # puts a glider on the grid
-        for cell in spaceship:
-            Cell.instances[cell].set_alive()
+        for cell in time_bomb:
+            (x,y) = cell
+            Cell.instances[(x + GRID_LEN/2, y + GRID_LEN/2)].set_alive()
         
     def run(self) -> None:
         """Main loop of game.
 
         Calls functions responsible for running game logic and drawing graphics every frame.
         """
+        clock = pygame.time.Clock()
         while True:
             self.process_game_logic()
             self.draw_game_elements()
+            #time.sleep(0.05)
+            clock.tick()
+            print(clock.get_fps())
 
     def process_game_logic(self):
         """Function for updating game logic every frame."""
@@ -190,14 +196,17 @@ class Game:
         #    file.write(f"CELL: {loc}:{cell.is_alive}: {cell.neighbors}:{cell.living_neighbors}\n")
 
     def draw_game_elements(self):
+        updates = []
         """Method to draw to pygame screen every frame"""
         while len(Cell.changing) > 0:
             cell, _ = Cell.changing.pop(0)
-            cell.draw(self.screen)
+            updates.append(cell.draw(self.screen))
+        pygame.display.update(updates)
 
     def handle_input(self):
         pass
 
 if __name__ == "__main__":
     game = Game()
+    input()
     game.run()
