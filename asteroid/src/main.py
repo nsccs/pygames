1bc411ac5657fa20663981382724fddaa377569c
drew_eso@outlook.com <drewolson@Drews-MacBook-Air.local>
2023-11-17 20:42:27 -0800

    Added velocity as a one-dimensional vector for now. Gave the ship an angle for direction and made the sprite rotateable.

--- asteroid/src/main.py
+++ asteroid/src/main.py
@@ -7,17 +7,43 @@ TOP_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 class Ship:
 # TODO: Split into higher level class that takes in asset as argument (?)
-    __slots__ = 'screen', 'pos', 'sprite', 'sprite_rect'
+    __slots__ = 'screen', 'pos', 'sprite', 'displayed_sprite', 'sprite_rect', \
+                'velocity_magnitude', 'direction', 'total_sprite_rotation', 'constant_deceleration'
     def __init__(self, screen):
         self.screen = screen
         self.sprite = pygame.image.load(os.path.join(TOP_DIR, 'assets/ship.png'))
+        self.displayed_sprite = self.sprite
         self.sprite_rect = self.sprite.get_rect()
-        self.pos = pygame.Vector2((screen.get_width() / 2) - self.sprite_rect.centerx, (screen.get_height() / 2) - self.sprite_rect.centery)
+        
+        self.pos = pygame.Vector2((screen.get_width() / 2) - self.sprite.get_rect().centerx, (screen.get_height() / 2) - self.sprite.get_rect().centery)
+        self.velocity_magnitude = 0
+        self.direction = math.pi / 2 # radians
+        self.total_sprite_rotation = 0 # degrees
+        self.constant_deceleration = -10
 
+    def rotate_sprite(self):
+        self.displayed_sprite = pygame.transform.rotate(self.sprite, self.total_sprite_rotation)
 
-    def move_forward(self):
-        pass
+    def rotate(self, angle):
+        self.direction += math.radians(angle)
+        self.total_sprite_rotation += angle
+        self.rotate_sprite()
 
+    def calc_vector_from_angle_and_magnitude(self, magnitude):
+        x = magnitude * math.cos(self.direction)
+        y = magnitude * -1 * math.sin(self.direction)
+        return pygame.Vector2(x, y)
+
+    def move_forward(self, amount):
+        pos_change_vector = self.calc_vector_from_angle_and_magnitude(amount)
+        self.pos += pos_change_vector
+
+    def slow_down(self, dt):
+        self.velocity_magnitude += self.constant_deceleration * dt
+        if self.velocity_magnitude < 0:
+            self.velocity_magnitude = 0
+
+    
 
 class Game:
     """Game class encapsulates functionality to make the game run."""
@@ -25,7 +51,7 @@ class Game:
     # slots attribute allows one to explicitly state expected attributes
     # benefits: faster access and space (memory) savings
     # update as needed
-    __slots__ = 'screen', 'dt', 'clock', 'shipvelocity', 'ship'
+    __slots__ = 'screen', 'dt', 'clock', 'ship'
 
     def __init__(self, title: str = "NSCCSC Asteroid Clone") -> None:
         """Init Game with initial pygame, display caption, and display size."""
@@ -38,12 +64,11 @@ class Game:
 
     def run(self) -> None:
         while True:
-            # self.handle_events()
             self.handle_input()
             self.process_game_logic()
             self.draw_game_elements()
             self.dt = self.clock.tick(60) / 1000
-            
+
 
     def handle_input(self):
         """Implement and update docstring and return type"""
@@ -53,26 +78,32 @@ class Game:
                 pygame.quit()
                 quit()
 
-            # if event.type == pygame.MOUSEMOTION:
-            #     mouseloc = pygame.mouse.get_pos()
-            #     pygame.draw.circle(self.screen, (255, 255, 255), mouseloc, mouseloc[0] // 2, width=mouseloc[1] // 10)
-            #     pygame.display.update()
-
-            if event.type == pygame.K_w:
-                self.ship.move_forward()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.ship.velocity_magnitude += 20
+                if event.key == pygame.K_s:
+                    self.ship.velocity_magnitude -= 20
+                if event.key == pygame.K_LEFT:
+                    self.ship.rotate(30)
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rotate(-30)
 
     def process_game_logic(self):
         """Implement and update docstring and return type"""
-        
+        # self.ship.pos = self.ship.pos.move_towards(pygame.mouse.get_pos(), 1000 * self.dt)
+        # print(self.dt)
+        self.ship.move_forward(self.ship.velocity_magnitude * self.dt)
+        self.ship.slow_down(self.dt)
+        # if self.ship.velocity_magnitude:
+        #     self.ship.move_forward(self.ship.velocity_magnitude)
+        #     self.ship.slow_down()
+        # pass
 
     def draw_game_elements(self):
         """Implement and update docstring and return type"""
         self.screen.fill((0, 0, 0))
-        # pygame.draw.circle(self.screen, (255, 255, 255), self.ship.pos, 10)
-        self.screen.blit(self.ship.sprite, self.ship.pos)
+        self.screen.blit(self.ship.displayed_sprite, self.ship.pos)
         pygame.display.flip()
-        
-        # pygame.display.update()
 
 
 if __name__ == "__main__":
